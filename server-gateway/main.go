af1c741ae52bfeac66dfb647deb0ad41c5a09330
Ehsan N. Moosa <ehsan.code@nested.me>
2018-09-06 16:21:03 +0430

    fix bug

--- server-gateway/main.go
+++ server-gateway/main.go
@@ -1,19 +1,35 @@
 package main
 
 import (
+    "os"
+
     "go.uber.org/zap"
+    "go.uber.org/zap/zapcore"
     "gopkg.in/fzerorubigd/onion.v3"
 )
 
 var (
     _Config   *onion.Onion
     _BundleID string
-    _Log      zap.Logger
+    _Log      *zap.Logger
+    _LogLevel zap.AtomicLevel
 )
 
 func init() {
     _Config = readConfig()
     _BundleID = _Config.GetString("BUNDLE_ID")
+    // Initialize Logger
+    _LogLevel = zap.NewAtomicLevelAt(zap.DebugLevel)
+    zap.NewProductionConfig()
+    config := zap.NewProductionConfig()
+    config.Encoding = "console"
+    config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+    config.Level = _LogLevel
+    if v, err := config.Build(); err != nil {
+        os.Exit(1)
+    } else {
+        _Log = v
+    }
 
 }
 
