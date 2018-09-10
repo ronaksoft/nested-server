a3dc63cc4c303d5ff4a3d451a5d88d6232edb5ee
behrooz <ahadian.behroz@gmail.com>
2018-09-10 14:27:13 +0430

    fixing task/update_assignee and get by filter candidate

--- server-gateway/gateway_api/task/functions.go
+++ server-gateway/gateway_api/task/functions.go
@@ -393,14 +393,15 @@ func (s *TaskService) updateAssignee(requester *nested.Account, request *nestedG
 		return
 	}
 	if task.UpdateAssignee(requester.ID, accountIDs) {
-		task.UpdateMemberIDs()
+		task1 := s.Worker().Argument().GetTask(request, response)
+		task1.UpdateMemberIDs()
 		response.Ok()
 		if len(accountIDs) == 1 {
-			go s.Worker().Pusher().TaskAssigned(task)
-			go s.Worker().Pusher().TaskNewActivity(task, nested.TASK_ACTIVITY_ASSIGNEE_CHANGED)
+			go s.Worker().Pusher().TaskAssigned(task1)
+			go s.Worker().Pusher().TaskNewActivity(task1, nested.TASK_ACTIVITY_ASSIGNEE_CHANGED)
 		} else {
-			go s.Worker().Pusher().TaskAddedToCandidates(task, requester.ID, accountIDs)
-			go s.Worker().Pusher().TaskNewActivity(task, nested.TASK_ACTIVITY_CANDIDATE_ADDED)
+			go s.Worker().Pusher().TaskAddedToCandidates(task1, requester.ID, accountIDs)
+			go s.Worker().Pusher().TaskNewActivity(task1, nested.TASK_ACTIVITY_CANDIDATE_ADDED)
 		}
 	} else {
 		response.Error(nested.ERR_UNKNOWN, []string{"internal_error"})
