c708e72fcbf985119341d75c58784be0ed998325
behrooz <ahadian.behroz@gmail.com>
2018-11-10 10:29:54 +0330

    some fix

--- server-mta/install.sh
+++ server-mta/install.sh
@@ -46,17 +46,7 @@ postconf -F '*/*/chroot = n'
 postconf -e message_size_limit=50000000
 postconf -e smtp_tls_security_level=may
 
-# SASL SUPPORT FOR SERVERS
-#
-# The following options set parameters needed by Postfix to enable
-# Cyrus-SASL support for authentication of mail servers.
-#
-postconf -e smtp_sasl_auth_enable=yes
-postconf -e smtp_sasl_password_maps=hash:/etc/postfix/sasl_passwd
-# leaving this empty will allow Postfix to use anonymous and plaintext authentication
-#smtp_sasl_security_options=
 ############
-
 # SASL SUPPORT FOR CLIENTS
 # The following options set parameters needed by Postfix to enable
 # Cyrus-SASL support for authentication of mail clients.
@@ -65,20 +55,19 @@ postconf -e smtp_sasl_password_maps=hash:/etc/postfix/sasl_passwd
 postconf -e smtpd_sasl_auth_enable=yes
 postconf -e broken_sasl_auth_clients=yes
 postconf -e smtpd_recipient_restrictions=permit_sasl_authenticated,reject_unauth_destination
-
 # smtpd.conf
-cat >> /etc/sasl2/smtpd.conf <<EOF #/etc/postfix/sasl/smtpd.conf
+cat >> /etc/postfix/sasl/smtpd.conf <<EOF
 pwcheck_method: auxprop
 auxprop_plugin: sasldb
 mech_list: PLAIN LOGIN CRAM-MD5 DIGEST-MD5 NTLM
 EOF
-
 # sasldb2
 echo ${NST_SMTP_CRED} | tr , \\n > /tmp/passwd
 while IFS=':' read -r _user _pwd; do
   echo $_pwd | saslpasswd2 -p -c -u ${NST_DOMAIN} $_user
 done < /tmp/passwd
 chown postfix.sasl /etc/sasldb2
+
 
 ############
 # Enable TLS
